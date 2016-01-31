575748121e894a9744f9b37281168e58863fd7eb
Treyarched <shardflame@gmail.com>
2016-01-31 11:04:25 -0500

    Update Storage.py

--- Storage.py
+++ Storage.py
@@ -14,10 +14,10 @@ class Storage:
         self.asecret = self.dct['asecret']
         self.otoken = self.dct['otoken']
         self.osecret = self.dct['osecret']
-        self.mentions = self.dct['mentions']
-        self.owner = self.dct['owner']
-        self.d_key = self.dct['d_key']
-        
+        #self.mentions = self.dct['mentions']
+        #self.owner = self.dct['owner']
+        #self.d_key = self.dct['d_key']
+        #self.loader()
 
     ## Initializes Server Data File
     def loader(self,fname='data.pkl'):
@@ -36,7 +36,9 @@ class Storage:
 
 
     ## Saves Value to Storage
-    def flush(self,dct, fname = 'data.pkl'):
+    def flush(self, fname = 'data.pkl',dct = None):
+        if dct == None:
+            dct = self.dct
         pkl_file = open(fname,'wb')
         pickle.dump(dct,pkl_file)
         pkl_file.close()
